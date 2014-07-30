58de71b2df30df232e2add733da1c7e4d0db0a98
neonowy <krzysztof.zbudniewek@outlook.com>
2014-07-30 19:20:03 +0200

    Added packages and packages.json file generator.

---
+++ generate.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'json'
+
+def generate_list
+	packages = []
+	files = Dir.entries(Dir.pwd)
+	puts "Adding.."
+	files.each do |file|
+		if (file[-4,4] == "json" && file != "packages.json")
+			puts file[0..-6]
+			packages.push(file[0..-6])
+		end
+	end
+	puts packages.to_json
+	packages_file = open('packages.json', 'w')
+	packages_file.write(packages.to_json)
+	packages_file.close
+	puts "Done."
+end
+
+generate_list
